1b320f285bf97c10baa0d0710ef6c2388c6b2e66
taismoraes <taismoraes2@gmail.com>
2023-01-09 15:48:00 -0300

    fix workflow chromatic

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,5 +1,7 @@
 import { Button, ButtonProps } from "./Button";
 import { Meta, StoryObj } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 export default {
   title: "Components/Button",
@@ -8,6 +10,17 @@ export default {
     children: "Create Account",
   },
   argTypes: {},
+  decorators: [withDesign],
 } as Meta<ButtonProps>;
 
 export const Default: StoryObj<ButtonProps> = {};
+
+const Template: ComponentStory<typeof Button> = () => <Button>Teste</Button>;
+
+export const Example = Template.bind({});
+Example.parameters = {
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/y6CIvws1rHXJoWq0q2hgt2/Ignite-Lab-Design-System?node-id=1%3A170&t=LsEKNGgv7Ie5HLJK-0",
+  },
+};
